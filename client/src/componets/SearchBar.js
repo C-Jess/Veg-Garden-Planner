91b19c498195410362d5938463be1b2e5c4600fb
Jessica Christensen <65625958+C-Jess@users.noreply.github.com>
2022-05-31 16:20:39 +0100

    added search

--- client/src/componets/SearchBar.js
+++ client/src/componets/SearchBar.js
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import "./SearchBar.css";
 
 function SearchBar({ placeholder, data }) {
+  const [filteredData, setfilteredData] = useState([]);
+  const handleFilter = (event) => {
+    const searchWord = event.target.value;
+    const newFilter = data.filter((value) => {
+      return value.plant_name.toLowerCase().includes(searchWord.toLowerCase());
+    });
+    if (searchWord === "") {
+      setfilteredData([]);
+    } else {
+      setfilteredData(newFilter);
+    }
+  };
+
   return (
     <div className="search">
       <div className="seachInputs">
-        <input type="text" placeholder={placeholder} />
+        <input type="text" placeholder={placeholder} onChange={handleFilter} />
       </div>
-      <div className="results">
-        {!data
-          ? null
-          : data.map((value, key) => {
-              return <div> {value.plant_name} </div>;
-            })}
-      </div>
+      {filteredData.length !== 0 && (
+        <div className="results">
+          {!data
+            ? null
+            : filteredData.slice(0, 15).map((value, key) => {
+                return (
+                  <div className="item">
+                    {" "}
+                    <p>{value.plant_name}</p>{" "}
+                  </div>
+                );
+              })}
+        </div>
+      )}
     </div>
   );
 }
