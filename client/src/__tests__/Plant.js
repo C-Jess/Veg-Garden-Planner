c7c973d8fc09bfa574b9c02572589cfc8a384308
Jessica Christensen <65625958+C-Jess@users.noreply.github.com>
2022-06-02 19:40:21 +0100

    added button tests for plant component

--- client/src/__tests__/Plant.js
+++ client/src/__tests__/Plant.js
@@ -3,7 +3,12 @@ import { render, screen } from "@testing-library/react";
 import Plant from "../componets/Plant";
 
 const testData = [
-  { id: 1, name: "Sprouting Broccoli", protection: "Inside", offset: -3 },
+  {
+    id: 1,
+    name: "Sprouting Broccoli",
+    protection: "Outside: Protected",
+    offset: -3,
+  },
   { id: 5, name: "Leeks", protection: "Outside: Unprotected", offset: -4 },
   { id: 14, name: "Peppers", protection: "Inside", offset: -7 },
 
@@ -15,11 +20,7 @@ const testData = [
   },
 ];
 
-it("renders without crashing", () => {
-  render(<Plant plant={testData[0]} />);
-});
-
-describe("passing list", () => {
+describe("renders without crashing", () => {
   var nameTestData = [];
   testData.forEach((element) => {
     nameTestData.push([element.name, element]);
@@ -27,5 +28,27 @@ describe("passing list", () => {
   test.each(nameTestData)("%s plant renders", (name, plant) => {
     render(<Plant plant={plant} />);
     expect(screen.getByText(name, { exact: false })).toBeInTheDocument();
+  });
+});
+
+describe("delete button is rendered", () => {
+  var deleteButtonTestData = [];
+  testData.forEach((element) => {
+    deleteButtonTestData.push([element.name, element]);
+  });
+  test.each(deleteButtonTestData)("%s button renders", (name, plant) => {
+    render(<Plant plant={plant} />);
+    expect(screen.getAllByRole("button")[0].textContent).toBe("Delete");
+  });
+});
+
+describe("edit button is rendered", () => {
+  var deleteButtonTestData = [];
+  testData.forEach((element) => {
+    deleteButtonTestData.push([element.name, element]);
+  });
+  test.each(deleteButtonTestData)("%s button renders", (name, plant) => {
+    render(<Plant plant={plant} />);
+    expect(screen.getAllByRole("button")[1].textContent).toBe("Edit");
   });
 });
