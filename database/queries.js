e5a3d880b53faa69c83ae0c3096c6f616ccadf24
Jessica Christensen <65625958+C-Jess@users.noreply.github.com>
2022-06-04 15:18:02 +0100

    dont reject unauthorised

--- database/queries.js
+++ database/queries.js
@@ -1,5 +1,7 @@
 const { pool } = require("./config");
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
 // get all plant names
 const getPlants = (request, response) => {
   pool.query("SELECT * FROM plants", (error, results) => {
